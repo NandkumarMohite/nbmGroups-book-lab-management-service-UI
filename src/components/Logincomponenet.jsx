f01e8d0056d30361473fb8c6e5df552b7cf2f54c
NandkumarMohite <n.b.mohite88@gmail.com>
2023-01-11 21:12:19 +0530

    fix(UI): added event handling in sign up

--- src/components/Logincomponenet.jsx
+++ src/components/Logincomponenet.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Logincomponenet() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+
+  const navigate = useNavigate();
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -31,9 +35,12 @@ function Logincomponenet() {
     }).then((result) => {
       result.json().then((Response) => {
         if (Response.emailId != null) {
-          alert("Signed In");
+          console.log(Response.emailId)
+          // alert("Signed In");
           localStorage.setItem('UserInformation', JSON.stringify(Response))
-          window.location.reload()
+          navigate("/")
+        } else {
+          alert("no accout Signed In");
         }
       })
     })
@@ -43,54 +50,26 @@ function Logincomponenet() {
 
   return (
     <>
-      <form id="msform" style={{marginTop:"200px"}}>
-        <fieldset id="p1">
-          <h2 class="fs-title">Create your account</h2>
-          <h3 class="fs-subtitle">This is step 1</h3>
-          <input type="text" value={email}
-            onChange={handleEmailChange}
-            placeholder="Enter your email" />
-          <input type="password" value={password}
-            onChange={handlePasswordChange} placeholder="Enter your password" />
-          <p className="forgot" align="center">
-            <a href="#">Forgot Password?</a>
-          </p>
-          <a href="#"></a>
-          <input type="button" name="Sign In" class="next action-button" value="Sign In" onClick={handleClick} />
-
-        </fieldset>
-      </form>
-      {/* <div className="LoginPage">
-          <div className="main">
-            <p className="sign" align="center">
-              Login{" "}
+      <div className="loginform">
+        <form id="msform" style={{ marginTop: "200px" }}>
+          <fieldset id="p1">
+            <h2 class="fs-title">Create your account</h2>
+            <h3 class="fs-subtitle">This is step 1</h3>
+            <input type="text" value={email}
+              onChange={handleEmailChange}
+              placeholder="Enter your email" />
+            <input type="password" value={password}
+              onChange={handlePasswordChange} placeholder="Enter your password" />
+            <p className="forgot" align="center">
+              <a href="#">Forgot Password?</a>
             </p>
-            <form className="form1">
-              <input
-                className="un "
-                type="email"
-                align="center"value={email}
-                onChange={handleEmailChange} placeholder="Email Id" 
-              />
-              <input
-                className="pass"
-                type="password"
-                align="center"value={password}
-                onChange={handlePasswordChange} placeholder="Password"
-              />
-              <a className="submit" align="center" onClick={handleClick}>
-                Login
-              </a>
-              <p className="forgot" align="center">
-                <a href="#">Forgot Password?</a>
-              </p>
-              <a href="#"></a>
-            </form>
-          </div>
-          <a href="#"></a>
-        </div>
-         */}
+            <a href="#"></a>
+            <input type="button" name="Sign In" class="next action-button" value="Sign In" onClick={handleClick} />
 
+          </fieldset>
+        </form>
+      </div>
+      
     </>
   );
 }
--- src/components/Signupcomponents.jsx
+++ src/components/Signupcomponents.jsx
@@ -7,6 +7,7 @@ function Signupcomponent() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordcheck, setPasswordcheck] = useState("");
     const [FirstName, setFirstName] = useState("");
     const [LastName, setLastName] = useState("");
     const [MobileNumber, setMobileNumber] = useState("");
@@ -24,6 +25,9 @@ function Signupcomponent() {
 
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
+    };
+    const handlePasswordCheckChange = (e) => {
+        setPasswordcheck(e.target.value);
     };
     const handleFirstNameChange = (e) => {
         setFirstName(e.target.value);
@@ -52,25 +56,25 @@ function Signupcomponent() {
     const handleTalukaChange = (e) => {
         setTaluka(e.target.value);
     };
-   
-    function next(){
+
+    function next() {
         document.getElementById("p1").style.display = "none";
         document.getElementById("p2").style.display = "block";
         document.getElementById("bar2").style.color = "#fc5c65";
-       
+
     }
-    function next2(){
+    function next2() {
         document.getElementById("p2").style.display = "none";
         document.getElementById("p3").style.display = "block";
         document.getElementById("bar3").style.color = "#fc5c65";
     }
-    function Previous(){
+    function Previous() {
         document.getElementById("p1").style.display = "block";
         document.getElementById("p2").style.display = "none";
         document.getElementById("bar2").style.color = "deepskyblue";
-        
+
     }
-    function Previous2(){
+    function Previous2() {
         document.getElementById("p2").style.display = "block";
         document.getElementById("p3").style.display = "none";
         document.getElementById("bar3").style.color = "deepskyblue";
@@ -93,80 +97,97 @@ function Signupcomponent() {
     }
 
     function handleClick() {
+        if(password===passwordcheck){
+        if (jsonData.emailId === "") {
+            alert("emailId can't be empty")
+            
+        }
+        else if(jsonData.password === ""){
+            alert("password can't be empty")
+        }
+        else if(jsonData.firstName === ""){
+            alert("firstName can't be empty")
+        }
+        else if(jsonData.lastName === ""){
+            alert("lastName can't be empty")
+        }
+        else {
+            fetch('http://localhost:8888/signup', {
 
-        fetch('http://localhost:8888/signup', {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'accept': 'application/json'
+                },
+                body: JSON.stringify(jsonData)
 
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': 'application/json'
-            },
-            body: JSON.stringify(jsonData)
-
-        })
-
+            })
+        }
+    }else{
+        alert("Check Your password not maching ")
+    }
     }
 
     return (
         <>
-
-            <div class="multilevel">
-                <form id="msform">
-                    {/* <!-- progressbar --> */}
-                    <ul id="progressbar">
-                        <li id="bar1" style={{color:"#fc5c65"}}>Account Setup</li>
-                        <li id="bar2">Social Profiles</li>
-                        <li id="bar3">Personal Details</li>
-                    </ul>
-                    {/* <!-- fieldsets --> */}
-                    <fieldset id="p1">
-                        <h2 class="fs-title">Create your account</h2>
-                        <h3 class="fs-subtitle">This is step 1</h3>
-                        <input type="text"  value={email}
-                            onChange={handleEmailChange}
-                            placeholder="Enter your email" />
-                        <input type="text" value={MobileNumber}
-                            onChange={handleMobileNumberChange} placeholder="Mobile Number" />
-                        <input type="password" value={password}
-                            onChange={handlePasswordChange} placeholder="Enter your password" />
+            <div className="signup">
+                <div class="multilevel">
+                    <form id="msform">
+                        {/* <!-- progressbar --> */}
+                        <ul id="progressbar">
+                            <li id="bar1" style={{ color: "#fc5c65" }}>Account Setup</li>
+                            <li id="bar2">Social Profiles</li>
+                            <li id="bar3">Personal Details</li>
+                        </ul>
+                        {/* <!-- fieldsets --> */}
+                        <fieldset id="p1">
+                            <h2 class="fs-title">Create your account</h2>
+                            <h3 class="fs-subtitle">This is step 1</h3>
+                            <input type="text" value={email}
+                                onChange={handleEmailChange}
+                                placeholder="Enter your email" />
+                            <input type="text" value={MobileNumber}
+                                onChange={handleMobileNumberChange} placeholder="Mobile Number" />
                             <input type="password" value={password}
-                            onChange={handlePasswordChange} placeholder="Confirm your password" />
-                        <input type="button" name="next" class="next action-button" value="Next" onClick={next}/>
-                    </fieldset>
-                    <fieldset id="p2">
-                        <h2 class="fs-title">Social Profiles</h2>
-                        <h3 class="fs-subtitle">Your presence on the social network</h3>
-                        <input type="text" value={Nationality}
-                            onChange={handleNationalityChange} placeholder="Nationality" />
-                        <input type="text" value={State}
-                            onChange={handleStateChange} placeholder="State" />
-                        <input type="text" value={District}
-                            onChange={handleDistrictChange} placeholder="District" />
-                        <input type="text" value={Taluka}
-                            onChange={handleTalukaChange} placeholder="Taluka" />
-                        <input type="button" name="previous" class="previous action-button" value="Previous" onClick={Previous} />
-                        <input type="button" name="next" class="next action-button" value="Next" onClick={next2}/>
-                    </fieldset>
-                    <fieldset id="p3">
-                        <h2 class="fs-title">Personal Details</h2>
-                        <h3 class="fs-subtitle">We will never sell it</h3>
-                        <input type="text"  value={FirstName}
-                            onChange={handleFirstNameChange} placeholder="First Name" />
-                        <input type="text" value={LastName}
-                            onChange={handleLastNameChange} placeholder="Last Name" />
-                        <input type="text" value={DateOfBirth}
-                            onChange={handleDateOfBirthChange} placeholder="DOB" />
+                                onChange={handlePasswordChange} placeholder="Enter your password" />
+                            <input type="password" value={passwordcheck}
+                                onChange={handlePasswordCheckChange} placeholder="Confirm your password" />
+                            <input type="button" name="next" class="next action-button" value="Next" onClick={next} />
+                        </fieldset>
+                        <fieldset id="p2">
+                            <h2 class="fs-title">Social Profiles</h2>
+                            <h3 class="fs-subtitle">Your presence on the social network</h3>
+                            <input type="text" value={Nationality}
+                                onChange={handleNationalityChange} placeholder="Nationality" />
+                            <input type="text" value={State}
+                                onChange={handleStateChange} placeholder="State" />
+                            <input type="text" value={District}
+                                onChange={handleDistrictChange} placeholder="District" />
+                            <input type="text" value={Taluka}
+                                onChange={handleTalukaChange} placeholder="Taluka" />
+                            <input type="button" name="previous" class="previous action-button" value="Previous" onClick={Previous} />
+                            <input type="button" name="next" class="next action-button" value="Next" onClick={next2} />
+                        </fieldset>
+                        <fieldset id="p3">
+                            <h2 class="fs-title">Personal Details</h2>
+                            <h3 class="fs-subtitle">We will never sell it</h3>
+                            <input type="text" value={FirstName}
+                                onChange={handleFirstNameChange} placeholder="First Name" />
+                            <input type="text" value={LastName}
+                                onChange={handleLastNameChange} placeholder="Last Name" />
+                            <input type="text" value={DateOfBirth}
+                                onChange={handleDateOfBirthChange} placeholder="DOB" />
                             <input type="text" value={Gender}
-                            onChange={handleGenderChange} placeholder="gender" />
-                       
-                        <input type="button" name="previous" class="previous action-button" value="Previous" onClick={Previous2} />
-                        <a class="submit action-button" target="_top" onClick={handleClick}>Submit</a>
-                    </fieldset>
-                </form>
+                                onChange={handleGenderChange} placeholder="gender" />
+
+                            <input type="button" name="previous" class="previous action-button" value="Previous" onClick={Previous2} />
+                            <a class="submit action-button" target="_top" onClick={handleClick}>Submit</a>
+                        </fieldset>
+                    </form>
+                </div>
+
             </div>
-
-
 
 
 
